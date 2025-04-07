02bbba92c64d56db999654a05e30513447a5c51e
Rahul <amaleexxii@gmail.com>
2025-04-07 12:01:58 +0530

    implimented binary search tree

--- test.js
+++ test.js
@@ -1,69 +1,111 @@
-class Node1{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+class Node1 {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
-    constructor(){
-        this.head = null;
-        this.tail = null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  insertAtEnd(value) {
+    const newNode = new Node1(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
     }
-    insertAtEnd(value){
-        const newNode = new Node1(value)
-        if(!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-            return
-        }
-        this.tail.next = newNode
-        this.tail = newNode
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  insertAtStart(value) {
+    const newNode = new Node1(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
     }
-    insertAtStart(value){
-        const newNode = new Node1(value)
-        if(!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-            return
-        }
-        newNode.next = this.head
-        this.head = newNode
-    }
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
 
-class Node2{
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }
+class Node2 {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
 }
-class DoublyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  insertAtStart(value) {
+    const newNode = new Node2(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
     }
-    insertAtStart(value){
-        const newNode = new Node2(value)
-        if(!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-            return
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+  }
+  insertAtEnd(value) {
+    const newNode = new Node2(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
+}
+
+// B S T
+
+class Node3 {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node3(value);
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+    let current = this.root;
+    while (true) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
         }
-        newNode.next = this.head
-        this.head.prev = newNode
-        this.head = newNode
-    }
-    insertAtEnd(value){
-        const newNode = new Node2(value)
-        if(!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-            return
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          return;
         }
-        this.tail.next = newNode
-        newNode.prev = this.tail
-        this.tail = newNode
+        current = current.right;
+      } else {
+        console.log("element is already exists");
+      }
     }
+  }
 }
